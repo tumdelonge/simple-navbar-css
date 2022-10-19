aeb0f016a39f9e891a0e260a76d7ce66ac2e477a
tumdelonge <tumdelonge@gmail.com>
2022-10-19 22:54:08 +0700

    navbar_with_css

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Staatliches&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <a href="" class="logo-nvgs">
+                <div class="logo-1">
+                    <p>we</p>
+                </div>
+                <div class="logo-2">
+                    <p>9row</p>
+                </div>
+                <div class="logo-3">
+                    <p>.io</p>
+                </div>
+            </a>
+            <div class="btn-nvgs">
+                <ul>
+                    <li><a href="">Beranda</a></li>
+                    <li><a href="">Profil</a></li>
+                    <li><a href="">Layanan</a></li>
+                    <li><a href="">Tentang</a></li>
+                </ul>
+                </a>
+            </div>
+            <a href="" class="btn-box">Lorem, ipsum.</a>
+        </div>
+    </div>
+</body>
+
+</html>
